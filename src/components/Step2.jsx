cc33cf72dd40d654e430c2fe08a6291156b09941
Valery <escapism369@gmail.com>
2022-12-30 01:11:44 +0500

    Вёрстка 2 и 3 шага, интерактив, логика (без запоминания информации)

--- src/components/Step2.jsx
+++ src/components/Step2.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CardForm from './CardForm';
 import MyForm from './MyForm';
@@ -7,11 +7,62 @@ import PrevButton from './PrevButton';
 
 const Step2 = () => {
 	const navigate = useNavigate();
+
+	const [periodPrice, setPeriodPrice] = useState([9, 12, 15]);
+
 	const NextStep = () => {
 		navigate("/step3");
 	}
 	const PrevStep = () => {
 		navigate("/");
+	}
+
+	const ShowPeriod = (e) => {
+		const monthly = document.querySelector('.plan-period-monthly');
+		const yearly = document.querySelector('.plan-period-yearly');
+		const periodSwitch = document.querySelector('.plan-period__switch');
+		const freeMonth = document.querySelectorAll('.plan-form__free');
+
+		switch (e.target.classList.value) {
+			case 'plan-period__title plan-period-monthly':
+				monthly.classList.add('active');
+				yearly.classList.remove('active');
+				periodSwitch.classList.remove('active');
+				for (let i = 0; i < freeMonth.length; i++) {
+					freeMonth[i].classList.remove('active');
+				}
+				setPeriodPrice([9, 12, 15]);
+				break;
+			case 'plan-period__title plan-period-yearly':
+				monthly.classList.remove('active');
+				yearly.classList.add('active');
+				periodSwitch.classList.add('active');
+				for (let i = 0; i < freeMonth.length; i++) {
+					freeMonth[i].classList.add('active');
+				}
+				setPeriodPrice([90, 120, 150]);
+				break;
+			case 'plan-period__switch':
+				monthly.classList.remove('active');
+				yearly.classList.add('active');
+				periodSwitch.classList.add('active');
+				for (let i = 0; i < freeMonth.length; i++) {
+					freeMonth[i].classList.add('active');
+				}
+				setPeriodPrice([90, 120, 150]);
+				break;
+			case 'plan-period__switch active':
+				monthly.classList.add('active');
+				yearly.classList.remove('active');
+				periodSwitch.classList.remove('active');
+				for (let i = 0; i < freeMonth.length; i++) {
+					freeMonth[i].classList.remove('active');
+				}
+				setPeriodPrice([9, 12, 15]);
+				break;
+			default:
+				break;
+		}
 	}
 	return (
 		<CardForm active={2}>
@@ -20,7 +71,37 @@ const Step2 = () => {
 					title={"Select your plan"}
 					description={"You have the option of monthly or yearly billing."}
 				>
-
+					<div className="form__plan plan-form">
+						<label>
+							<input type="radio" name='plan' className='plan-form__radio' defaultChecked />
+							<div className="plan-form__item">
+								<h4 className='plan-form__title'>Arcade</h4>
+								<p className="plan-form__price">${periodPrice[0]}/mo</p>
+								<p className="plan-form__free">2 months free</p>
+							</div>
+						</label>
+						<label>
+							<input type="radio" name='plan' className='plan-form__radio' />
+							<div className="plan-form__item">
+								<h4 className='plan-form__title'>Advanced</h4>
+								<p className="plan-form__price">${periodPrice[1]}/mo</p>
+								<p className="plan-form__free">2 months free</p>
+							</div>
+						</label>
+						<label>
+							<input type="radio" name='plan' className='plan-form__radio' />
+							<div className="plan-form__item">
+								<h4 className='plan-form__title'>Pro</h4>
+								<p className="plan-form__price">${periodPrice[2]}/mo</p>
+								<p className="plan-form__free">2 months free</p>
+							</div>
+						</label>
+					</div>
+					<div className="form__plan-period plan-period" onClick={e => ShowPeriod(e)}>
+						<button className='plan-period__title plan-period-monthly active'>Monthly</button>
+						<button className='plan-period__switch'></button>
+						<button className='plan-period__title plan-period-yearly'>Yearly</button>
+					</div>
 				</MyForm>
 				<div className="buttons">
 					<PrevButton onClick={PrevStep}>Go Back</PrevButton>
--- src/components/Step3.jsx
+++ src/components/Step3.jsx
@@ -13,6 +13,12 @@ const Step3 = () => {
 	const PrevStep = () => {
 		navigate("/step2");
 	}
+
+	const ShowActive = (e, num) => {
+		let addonsItems = document.querySelectorAll('.addons-form__item');
+
+		addonsItems[num].classList.toggle('active');
+	}
 	return (
 		<CardForm active={3}>
 			<div className="cardform__container">
@@ -20,7 +26,47 @@ const Step3 = () => {
 					title={"Pick add-ons"}
 					description={"Add-ons help enhance your gaming experience."}
 				>
-
+					<div className="form__addons addons-form">
+						<label>
+							<div className="addons-form__item" onChange={e => ShowActive(e, 0)}>
+								<div className='addons-form__item-container'>
+									<input type="checkbox" name='addons' className='addons-form__check' />
+									<span className='addons-form__mark'></span>
+									<div className="addons-form__text">
+										<h4 className='addons-form__title'>Online service</h4>
+										<p className="addons-form__desc">Access to multiplayer games</p>
+									</div>
+								</div>
+								<p className="addons-form__price">+$1/mo</p>
+							</div>
+						</label>
+						<label>
+							<div className="addons-form__item" onChange={e => ShowActive(e, 1)}>
+								<div className='addons-form__item-container'>
+									<input type="checkbox" name='addons' className='addons-form__check' />
+									<span className='addons-form__mark'></span>
+									<div className="addons-form__text">
+										<h4 className='addons-form__title'>Larger storage</h4>
+										<p className="addons-form__desc">Extra 1TB of cloud save</p>
+									</div>
+								</div>
+								<p className="addons-form__price">+$2/mo</p>
+							</div>
+						</label>
+						<label>
+							<div className="addons-form__item" onChange={e => ShowActive(e, 2)}>
+								<div className='addons-form__item-container'>
+									<input type="checkbox" name='addons' className='addons-form__check' />
+									<span className='addons-form__mark'></span>
+									<div className="addons-form__text">
+										<h4 className='addons-form__title'>Customizable Profile</h4>
+										<p className="addons-form__desc">Custom theme on your profile</p>
+									</div>
+								</div>
+								<p className="addons-form__price">+$2/mo</p>
+							</div>
+						</label>
+					</div>
 				</MyForm>
 				<div className="buttons">
 					<PrevButton onClick={PrevStep}>Go Back</PrevButton>
